dca311b9d69ee8ee245f552a34057786abf549f5
Mariana Marcondes <mari.marcondes@outlook.com>
2021-06-23 20:35:52 -0300

    First try of validator.js and started index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import validator from './validator.js';
 
-console.log(validator);
+const button = document.getElementById("button");
+
+ function validate (){
+    let card = document.getElementById("cardNumber").value;
+    const validator = validator.isValid(card);
+     if (validator == true){
+
+     }
+     else (validator == false){
+   
+
+     } 
+ }
+  function masker (){
+     let cardNumber = document.getElementById("cardNumber").value;
+     const mask = validator.maskify(cardNumber);
+      document.getElementById("cardNumber").value = mask;
+  }
+//console.log(validator);
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,48 @@
 const validator = {
-  // ...
+  isValid: function (cardNumber) {
+    if (cardNumber == "" || cardNumber == null || cardNumber.length > 16) {
+      alert("Informação está incorreta ou não é suficiente.");
+      throw new TypeError("Some informations are wrong", "cipher.js", 7);
+    }
+
+    let cardValidate = "";
+    let cardArr = Array.from(cardNumber).reverse;
+
+    for (let i = 0; (i = cardArr.length - 1); i++){
+      if (i % 2 != 0) {
+        cardArr[i] = cardArr[i] * 2;
+      }
+
+    if (cardArr[i] > 9) {
+      let rest = cardArr[i] % 10;
+      let aprox = Math.trunc(cardArr[i] / 10);
+      let sum = rest + aprox;
+      cardArr[i] = sum;
+    }}
+
+    let total = cardArr.reduce(
+      (total, currentElement) => total + currentElement
+    );
+
+    if (total % 10 == 0) {
+      cardValidate = true;
+    } else {
+      cardValidate = false;
+    }
+
+    return cardValidate;
+  },
+
+  maskify: function (cardNumber) {
+    let digitMask = Array.from(cardNumber);
+
+    for (let i = 0; i < digitMask.length - 4; i++) {
+      digitMask[i] = "#";
+    }
+    let join = digitMask.join("");
+
+    return join;
+  },
 };
 
 export default validator;
